54aa564174a84e40618da7804bed97092a6b4a0f
andres-salcedopatroni <andres.salcedo@unmsm.edu.pe>
2024-06-17 00:33:27 -0500

    Chatbot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  ver:boolean= true;
+  
+  mostrar():void{
+    if(this.ver)
+      this.ver=false
+    else
+      this.ver=true
+  }
+
 }
