3fc2d4af40ae503987458ddcbc5f31ec368b97a6
andres-salcedopatroni <andres.salcedo@unmsm.edu.pe>
2024-06-17 01:24:13 -0500

    Chatbot Terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChatbotService } from './servicios/chatbot.service';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   ver:boolean= true;
-  
-  mostrar():void{
-    if(this.ver)
-      this.ver=false
-    else
-      this.ver=true
+  mensaje:string="";
+  mensajes:any=["Hola como puedo ayudarte"];
+
+
+  constructor(private servicioChatbot:ChatbotService) {
   }
+
+  enviar():void{
+    this.mensajes.push(this.mensaje)
+    this.servicioChatbot.chat(this.mensaje).subscribe(
+      (data)=>{
+        this.mensajes.push(data.respuesta)
+        this.mensaje="";
+      },
+      (err)=>{
+        this.mensaje="";
+      }
+    );
+
+  }
+
 
 }
